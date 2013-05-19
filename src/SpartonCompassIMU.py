3cfa3c70ec357abae688a3e504021e3a2835deb9
Cheng-Lung Lee <chenglung@gmail.com>
2013-05-19 18:17:53 -0400

    Adding option to set declination and set East as heading zero so we can use it directly as ROS Global Heading, with GPS-UTM x,y

--- src/SpartonCompassIMU.py
+++ src/SpartonCompassIMU.py
@@ -51,6 +51,7 @@ from geometry_msgs.msg import Pose2D
 import serial, string, math, time, calendar
 
 #import tf
+from tf.transformations import euler_from_quaternion , quaternion_from_euler
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion
 
@@ -78,18 +79,39 @@ if __name__ == '__main__':
     global D_Compass
     global myStr1
     rospy.init_node('SpartonDigitalCompassIMU')
-    Pos_pub = rospy.Publisher('imu/HeadingTrue', Pose2D)
+    Pos_pub  = rospy.Publisher('imu/HeadingTrue', Pose2D)
+    PosD_pub = rospy.Publisher('imu/HeadingTrue_degree', Pose2D)
     Imu_pub = rospy.Publisher('imu/data', Imu)
     SpartonPose2D=Pose2D()
     SpartonPose2D.x=float(0.0)
     SpartonPose2D.y=float(0.0)
+    SpartonPose2D_D=SpartonPose2D
     #Init D_Compass port
     D_Compassport = rospy.get_param('~port','/dev/ttyUSB0')
     D_Compassrate = rospy.get_param('~baud',115200)
-    # printmodulus set to 1 is 100 Hz. 2 : 50Hz 
+    #<!--printmodulus 60:10Hz 40:15~17  35:17~18Hz 30:21Hz 25:23~27Hz ,20: 30~35Hz,15:35~55Hz 10: 55~76 Hz,  5: 70~100 Hz, 1:70~100 Hz -->
     D_Compassprintmodulus = rospy.get_param('~printmodulus',1)
-    #Digital compass heading offset in degree
-    D_Compass_offset = rospy.get_param('~offset',0.)
+    
+    #http://www.ngdc.noaa.gov/geomag-web/#declination -->
+    # Detroit Magnetic declination           : 2013-06-10 	7 30' 6"  W changing by  2.7' W per year = -7 30'6"=-7.501666666666667 degree ,
+    # Oakland University Magnetic declination: 2013-06-10 	7 27' 46" W changing by  2.7' W per year =         =-7.462777777777778 degree
+    #http://www.firefightermath.org/index.php?option=com_content&view=article&id=56&Itemid=70
+    #in NED , North-East-Down system
+    #Magnetic = true + westerly declination , True= Magnetic - westerly declination
+    #Magnetic = true - easterly declination , True= Magnetic + easterly declination
+    
+    
+    #Digital compass heading offset in degree   
+    # D_Compass_offset = rospy.get_param('~offset',0.)
+    D_Compass_declination = rospy.get_param('~declination',-7.462777777777778)* (math.pi/180.0)
+    # By defaule IMU use Megnatic North as zero degree in Quaternion
+    # If we want to use it directly with GPS-UTM x,y as Global Heading, East is our zero 
+    D_Compass_UseEastAsZero = rospy.get_param('~UseEastAsZero',True)
+    
+    # use this try to control miss sync in USB-serial, when it happends, must restart
+    Checksum_error_limits   =rospy.get_param('~Checksum_error_limits', 10.)
+    checksum_error_counter=0
+    
     imu_data = Imu()
     imu_data = Imu(header=rospy.Header(frame_id="SpartonCompassIMU"))
     
@@ -106,9 +128,12 @@ if __name__ == '__main__':
                                                0, 1e-6, 0, 
                                                0, 0, 1e-6]
     myStr1='\r\n\r\nprinttrigger 0 set drop\r\n'
-    myStr2='printmask gyrop_trigger accelp_trigger or quat_trigger or yawt_trigger or time_trigger or set drop\r\n'
+    # this is with true north setting
+    # myStr2='printmask gyrop_trigger accelp_trigger or quat_trigger or yawt_trigger or time_trigger or set drop\r\n'
+    # this is output magnetic north 
+    myStr2='printmask gyrop_trigger accelp_trigger or quat_trigger or time_trigger or set drop\r\n'
         # set the number high to get lower update rate , the IMU data is 100Hz rate , the string is 130 byte with 10 bit/byte , the max sampling rate is 88Hz
-        # printmodulus=2 might give us around 40Hz update rate ,set printmodulus=1 should give you the max speed. ( with auto skiping )
+        # printmodulus 60:10Hz 40:15~17  35:17~18Hz 30:21Hz 25:23~27Hz ,20: 30~35Hz,15:35~55Hz 10: 55~76 Hz,  5: 70~100 Hz, 1:70~100 Hz
     myStr_printmodulus=('printmodulus %i set drop\r\n' % D_Compassprintmodulus  )
     myStr3='printtrigger printmask set drop\r\n'
 
@@ -132,7 +157,7 @@ if __name__ == '__main__':
 
         else:
                 #sned error no data in buffer error
-                rospy.logerr('[1]Received No data from DigitalCompass. Shutdown!')
+                rospy.logerr('[Sparton][1]Received No data from DigitalCompass. Shutdown!')
                 rospy.signal_shutdown('Received No data from DigitalCompass')
         D_Compass.write(myStr2) # send printmask
         data = D_Compass.readline()
@@ -154,18 +179,24 @@ if __name__ == '__main__':
                 rospy.loginfo('Digital Compass Setup Complete!')
         else:
                 #sned error no data in buffer
-                rospy.logerr('[2]Received No data from DigitalCompass. Shutdown!')
+                rospy.logerr('[Sparton][2]Received No data from DigitalCompass. Shutdown!')
                 rospy.signal_shutdown('Received No data from DigitalCompass')
 
-        #data = D_Compass.readline()
-        #Read in D_Compass
+        #### loop-back test only
         #Testdata='P:,%i,ap,-6.34,-22.46,1011.71,gp,0.00,0.00,-0.00,yt,342.53,q,0.98,-0.01,0.01,-0.15\n'
+        #          0  1  2  3     4      5       6  7    8    9     10 11     1213   14    15   16  
+        #Testdata='P:,%i,ap,-6.34,-22.46,1011.71,gp,0.00,0.00,-0.00,q,0.98,-0.01,0.01,-0.15\n'
+        #Testdata='P:,%i,ap,-6.34,-22.46,1011.71,gp,0.00,0.00,-0.00,q,1.,0.,0.,0.\n'
+        #          0  1  2  3     4      5       6  7    8    9    10 11   12    13   14  
         #i=0
+        ### end of loop-back test
         while not rospy.is_shutdown():
-            #read D_Compass line  , The data example $HCHDT,295.5,T*2B
-            #                                        [0]    [1] 
+            
+            ### loop-back test only
             #i+=1
             #D_Compass.write(Testdata % i) # send testdata and do loop-back in RS232 for debug
+            ### end of loop-back test
+            
             data = D_Compass.readline()
             #rospy.loginfo("Received a sentence: %s" % data)
 
@@ -176,14 +207,16 @@ if __name__ == '__main__':
             #DatatimeNow = rospy.get_rostime()
             DataTimeSec=rospy.get_time()
             fields = data.split(',')
-            #print fields[0]+fields[2]+fields[6]+fields[10]+fields[12] #P:apgpytq
+            #print fields[0]+fields[2]+fields[6]+fields[10] #P:apgpq
 
             try:
-                if len(fields)>16:
-                            if 'P:apgpytq' == (fields[0]+fields[2]+fields[6]+fields[10]+fields[12]):
+                if len(fields)>14:
+                            if 'P:apgpq' == (fields[0]+fields[2]+fields[6]+fields[10]):
 
                                 #      0  1 mSec 2  3Ax  4Ay     5Az     5  7Gx  8Gy  9G    10 11YawT 1213w  14x   15y  16z
                                 #data='P:,878979,ap,-6.34,-22.46,1011.71,gp,0.00,0.00,-0.00,yt,342.53,q,0.98,-0.01,0.01,-0.15'
+                                #data='P:,%i,ap,-6.34,-22.46,1011.71,gp,0.00,0.00,-0.00,q,0.98,-0.01,0.01,-0.15\n'
+                                #      0  1  2  3     4      5       6  7    8    9    10 11   12    13   14  
 
                                 Ax=float(fields[3])/1000.*9.81 # convert to m/s^2 from mg/s
                                 Ay=float(fields[4])/1000.*9.81
@@ -191,10 +224,10 @@ if __name__ == '__main__':
                                 Gx=float(fields[7]) * (math.pi/180.0) # convert to radians from degrees
                                 Gy=float(fields[8]) * (math.pi/180.0)
                                 Gz=float(fields[9]) * (math.pi/180.0)
-                                w =float(fields[13])
-                                x =float(fields[14])
-                                y =float(fields[15])
-                                z =float(fields[16])
+                                w =float(fields[11])
+                                x =float(fields[12])
+                                y =float(fields[13])
+                                z =float(fields[14])
                                 
                                 #imu_data.header.stamp = rospy.Time.now() # Should add an offset here
                                 imu_data.header.stamp = rospy.Time.from_sec(DataTimeSec-len(data)/11520.) # this is timestamp with a bit time offset 10bit per byte @115200bps
@@ -204,7 +237,37 @@ if __name__ == '__main__':
                                 imu_data.orientation.y = x
                                 imu_data.orientation.z = -z
                                 imu_data.orientation.w = w
+                                # right now already in ENU but still use 0 is still at Magnetic north
                                 
+                                
+                                #Convert to euler
+                                #(r,p,y)=euler_from_quaternion(quaternion, axes='sxyz')
+                                #print "Start here"
+                                #NED=  [x,y,z,w] ; print NED
+                                ENU=  [y,x,-z,w] ; #print ENU
+                                #angle_NED=euler_from_quaternion(NED, axes='sxyz'); print angle_NED
+                                angle_ENU=euler_from_quaternion(ENU, axes='sxyz'); #print angle_ENU
+                                
+                                #add 90 degree and -declination , ROS= angle_ENU +90 degree - declination
+                                angle_ROS=(angle_ENU[0],angle_ENU[1],angle_ENU[2]+math.pi/2.- D_Compass_declination);     #print angle_ROS
+                                q_ROS=quaternion_from_euler(angle_ROS[0],angle_ROS[1],angle_ROS[2], axes='sxyz');         #print q_ROS
+
+                                #(r, p, y) = euler_from_quaternion([imu_data.orientation.x, imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w])
+                                #print (r,p,y)
+                                # Heading = 90 degree + Yaw - Magnetic declination.
+                                #yaw_ros= math.pi/2. +y- D_Compass_declination
+                                #print (yaw_ros)
+                                #q = quaternion_from_euler(ai, aj, ak, axes='sxyz') ,ai, aj, ak : Euler's roll, pitch and yaw angles 
+                                
+                                if D_Compass_UseEastAsZero :
+                                
+                                    #(imu_data.orientation.x, imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w)=quaternion_from_euler(r, p, yaw_ros, axes='sxyz')
+                                    (imu_data.orientation.x, imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w)=q_ROS
+                                    #print (imu_data.orientation.x, imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w)
+                                    #print imu_data.orientation
+                                else:
+                                    # if not use East As Zero, just use compass's origional data
+                                    pass
                                 # again note NED to ENU converstion
                                 imu_data.angular_velocity.x = Gy
                                 imu_data.angular_velocity.y = Gx
@@ -216,17 +279,40 @@ if __name__ == '__main__':
 
                                 Imu_pub.publish(imu_data)
 
-                                SpartonPose2D.y=1000./(float(fields[1])-SpartonPose2D.x) # put update rate here for debug the update rate
-                                SpartonPose2D.x=float(fields[1]) # put mSec tick here for debug the speed
-                                SpartonPose2D.theta = wrapToPI(math.radians(90.-float(fields[11])-D_Compass_offset))
-
+                                #SpartonPose2D.y=1000./(float(fields[1])-SpartonPose2D.x) # put update rate here for debug the update rate
+                                #SpartonPose2D.x=float(fields[1]) # put mSec tick here for debug the speed
+                                #SpartonPose2D.theta = wrapToPI(math.radians(90.-float(fields[11])-D_Compass_offset))
+                                #SpartonPose2D.theta = wrapToPI(yaw_ros)
+                                SpartonPose2D.theta = wrapToPI(angle_ROS[2])
+                                #print SpartonPose2D.theta/math.pi *180.
                                 Pos_pub.publish(SpartonPose2D)
-
+                                SpartonPose2D_D.theta =SpartonPose2D.theta/math.pi *180.
+                                PosD_pub.publish(SpartonPose2D_D)
+                                
+                                # reset checksum_error_counter when you have good data
+                                checksum_error_counter=0
 
                             else:
-                                rospy.logerr("[3]Received a sentence but not correct. Sentence was: %s" % data)
+                                rospy.logerr("[Sparton][3]Received a sentence but not correct. Sentence was: %s" % data)
+                                # Usually when USB-Serial miss sync wiill cause error
+                                checksum_error_counter+=1
+                                if (checksum_error_counter > Checksum_error_limits ):
+                                    #shutdown DGPS node
+                                    #raise SystemExit, 0
+                                    rospy.logfatal('[Sparton Compass] Too much back to back checksumn error in Sparton Compass data. Shutdown!')
+                                    rospy.signal_shutdown('Too much back to back checksum error in Sparton Compass data')
+                                    
+
+
                 else:
-                        rospy.logerr("[4]Received a sentence but not correct. Sentence was: %s" % data)
+                        rospy.logerr("[Sparton][4]Received a sentence but not correct. Sentence was: %s" % data)
+                        # Usually when USB-Serial miss sync wiill cause error
+                        checksum_error_counter+=1
+                        if (checksum_error_counter > Checksum_error_limits ):
+                            #shutdown DGPS node
+                            #raise SystemExit, 0
+                            rospy.logfatal('[Sparton Compass] Too much back to back checksumn error in Sparton Compass data. Shutdown!')
+                            rospy.signal_shutdown('Too much back to back checksum error in Sparton Compass data')
 
             except ValueError as e:
                 rospy.logwarn("Value error, likely due to missing fields in the data messages.Sentence was: %s" % data)
